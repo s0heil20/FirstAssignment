39f943abc5decfe17c5479157ae274e328621058
s0heil20 <soheil.mh2000@gmail.com>
2020-03-16 20:02:18 +0330

    add a HelloWorld!

--- GithubTest/src/Main.java
+++ GithubTest/src/Main.java
@@ -2,5 +2,11 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        int a = 10;
+        int b = 10;
+
+    }
+    public static int add(int a,int b){
+        return a + b;
     }
 }
