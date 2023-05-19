999650656c7bc00f476fc9a920a450ca8f61700c
s0heil20 <soheil.mh2000@gmail.com>
2023-05-19 14:02:40 +0330

    add phone and email buttons

--- Mobile Design/FirstAssignment/app/src/main/java/com/example/firstassignment/MainActivity.java
+++ Mobile Design/FirstAssignment/app/src/main/java/com/example/firstassignment/MainActivity.java
@@ -2,9 +2,15 @@ package com.example.firstassignment;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.util.Log;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.gson.Gson;
@@ -18,6 +24,12 @@ public class MainActivity extends AppCompatActivity {
     private Toast welcomeToast;
     private static final int toastDuration = 100000;
     private List<Paragraph> paragraphs;
+    private String myPhoneNumber = "tel:09380663225";
+    private String myEmail = "soheil.mh2000@gmail.com,";
+    private LinearLayout paragraphListLayout;
+    private ImageButton callButton;
+    private ImageButton emailButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,7 +38,9 @@ public class MainActivity extends AppCompatActivity {
 
         // Parsing Json and showing the paragraphs!
         parseJsonStringToJavaObject("myInfo.json");
-
+        paragraphListLayout = findViewById(R.id.paragraph_list);
+        addParagraphsToLayout(paragraphListLayout);
+        configureCallAndEmailAndName();
         // Showing Toast!
 //        welcomeToast = Toast.makeText(MainActivity.this, "Welcome to my app!", Toast.LENGTH_LONG);
 //        CountDownTimer toastCountDown = new CountDownTimer(15000, 1500) {
@@ -49,6 +63,36 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    private void configureCallAndEmailAndName() {
+        callButton = findViewById(R.id.call_button);
+        callButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent callIntent = new Intent(Intent.ACTION_DIAL);
+                callIntent.setData(Uri.parse(myPhoneNumber));
+                callIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(callIntent);
+            }
+        });
+        emailButton = findViewById(R.id.email_button);
+        emailButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent selectorIntent = new Intent(Intent.ACTION_SENDTO);
+                selectorIntent.setData(Uri.parse("mailto:"));
+                String[] addresses = {myEmail};
+                final Intent emailIntent = new Intent(Intent.ACTION_SEND);
+                emailIntent.putExtra(Intent.EXTRA_EMAIL, addresses);
+                emailIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                emailIntent.setSelector(selectorIntent);
+                startActivity(Intent.createChooser(emailIntent, "Sending Email to..."));
+            }
+        });
+
+
+    }
+
+
     private void parseJsonStringToJavaObject(String assetJsonFileName){
         String jsonFileString = Utils.getJsonFromAssets(getApplicationContext(), assetJsonFileName);
         Gson gson = new Gson();
@@ -57,6 +101,21 @@ public class MainActivity extends AppCompatActivity {
         for (Paragraph paragraph : this.paragraphs) {
             System.out.println(paragraph.get_text());
         }
+    }
+
+    private void addParagraphsToLayout(LinearLayout paragraphListLayout) {
+        for (Paragraph paragraph : this.paragraphs) {
+            paragraphListLayout.addView(createParagraphView(paragraph));
+        }
+    }
+
+    private View createParagraphView(Paragraph paragraph){
+        View paragraphView = getLayoutInflater().inflate(R.layout.paragraph_layout, null, false);
+        TextView titleTextView = (TextView) paragraphView.findViewById(R.id.paragraph_title);
+        TextView textTextView = (TextView) paragraphView.findViewById(R.id.paragraph_text);
+        titleTextView.setText(paragraph.get_title());
+        textTextView.setText(paragraph.get_text());
+        return paragraphView;
     }
 
     private class ToastThread implements Runnable{
